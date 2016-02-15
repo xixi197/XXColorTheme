a232db7bf8c023eeff1391513f409df7e0cabad4
lipeining <lipeining@kanjian.com>
2016-02-15 11:06:45 +0800

    nav color

--- Classes/Style/UIViewStyle.swift
+++ Classes/Style/UIViewStyle.swift
@@ -39,7 +39,7 @@ public extension UIView {
             xx_backgroundColorStyle = .Clear
 
         case .NavigationMaskBack:
-            xx_backgroundColorStyle = .Light4
+            xx_backgroundColorStyle = .Dark4
         case .NavigationSep:
             xx_backgroundColorStyle = .Light5
 
